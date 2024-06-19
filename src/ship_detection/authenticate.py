4517110652b1df2c4ae94aa457ded0edb5ad004e
Zuzanna Slobodzian <zuzanna.slobodzian@gmail.com>
2024-06-19 13:17:21 +0200

    changing auth

--- src/ship_detection/authenticate.py
+++ src/ship_detection/authenticate.py
@@ -4,10 +4,6 @@ from google.auth.transport.requests import Request
 import streamlit
 
 def authorize():
-    token_file = "config.json"
-    with open(token_file, "r") as f:
-        credentials = json.load(f)
-
     creds = credentials_lib.Credentials(
         token = streamlit.secrets["token"],
         refresh_token = streamlit.secrets["refresh_token"],
@@ -16,19 +12,5 @@ def authorize():
         client_secret = streamlit.secrets["client_secret"],
         scopes = streamlit.secrets["scopes"]
     )
-
-    # refreshing token and generate new (if this is necessary)
-    if creds and creds.expired and creds.refresh_token:
-        creds.refresh(Request())
-
-        with open(token_file, "w") as f:
-            json.dump({
-                'token': creds.token,
-                'refresh_token': creds.refresh_token,
-                'token_uri': creds.token_uri,
-                'client_id': creds.client_id,
-                'client_secret': creds.client_secret,
-                'scopes': creds.scopes
-            }, f)
 
     return creds
